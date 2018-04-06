cf213e9b97bacd70b69161e932c096a8a44cc7fa
Ken <kenkilmartin@gmail.com>
2018-04-06 01:02:42 +0100

    these starting get less meaningful just trying different number on setUser

--- src/DataFixtures/LoadReviews.php
+++ src/DataFixtures/LoadReviews.php
@@ -39,7 +39,7 @@ class LoadReviews extends Fixture implements DependentFixtureInterface
             $manager->persist($reviews);
 
             $reviews->setPort($this->getReference(LoadPorts::Port_REFERENCE.mt_rand(0,19)));
-            $reviews->setUser ($this->getReference(LoadUsers::AUTHOR_REFERENCE.mt_rand(0,19)));                                                           //($reviewedBy[mt_rand(0, count($reviewedBy)-1)]);
+            $reviews->setUser ($this->getReference(LoadUsers::AUTHOR_REFERENCE.mt_rand(20,23)));                                                           //($reviewedBy[mt_rand(0, count($reviewedBy)-1)]);
             $reviews->setDate($this->randomDate());
             $reviews->setReview($review[mt_rand(0,count($review)-1)]);
             $reviews->setPlaceOfPurchase($placeOfPurchase[mt_rand(0,count($placeOfPurchase)-1)]);
